764237daa54ad7afeaf25b6f7f3f7b9f77bc8aee
Anhtuan Tran <anhtuantran.github@gmail.com>
2021-01-11 19:44:17 -0600

    Completed header

--- js/index.js
+++ js/index.js
@@ -40,3 +40,51 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// nav
+const headerNav = document.querySelectorAll('a');
+headerNav[0].textContent = siteContent.nav["nav-item-1"];
+headerNav[1].textContent = "Product";
+headerNav[2].textContent = "Vision";
+headerNav[3].textContent = "Features";
+headerNav[4].textContent = "About";
+headerNav[5].textContent = "Contact";
+headerNav.forEach(link => link.style.color = "green");
+
+//header
+const firstSection = document.querySelector('.cta-text');
+const largeText = firstSection.querySelector('h1');
+const firstButton = firstSection.querySelector('button');
+largeText.innerHTML = "DOM <br> IS <br> AWESOME";
+firstButton.textContent = 'Get Started';
+
+
+//Image of a code snippet
+const codeSnippet = document.querySelector('#cta-img');
+codeSnippet.src = "img/header-img.png";
+
+//Main content
+const mainContent = document.querySelector(".main-content");
+const topContent = mainContent.querySelector(".text-content");
+
+const firstH4 = topContent.querySelector("h4");
+firstH4.textContent = siteContent["main-content"]["features-h4"];
+const firstP = topContent.querySelector("p");
+firstP.textContent = siteContent["main-content"]["features-content"];
+
+const secondH4 = topContent.querySelector("h4:nth-of-type(2)");
+secondH4.textContent = siteContent["main-content"]["about-h4"];
+const secondP = topContent.querySelector("p:nth-of-type(2)")
+secondP.textContent = siteContent["main-content"]["about-content"];
+
+
+//Middle Image
+const middleImage = document.querySelector(".middle-img #middle-img");
+middleImage.src = siteContent["main-content"]["middle-img-src"];
+
+//Bottom content
+const bottomContent = mainContent.querySelector(".bottom-content");
+const textContent = bottomContent.querySelector(".text-content");
+
+const bottomH4 = textContent.querySelector("h4");
+bottomH4.textContent = siteContent["main-content"]["services-h4"];
